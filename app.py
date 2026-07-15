6f4fb771eac0a1a4ca359d4cb5f16135ca279797
MusangQim <musangqim03@gmail.com>
2026-07-15 13:29:14 +0800

    UPDATE: Remove type annotation, create new environment in the folder project

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 
 @app.route('/lookup', methods=['POST'])
-def lookup() -> None:
+def lookup():
     data = request.get_json()
     username = data['username']
     return jsonify({
@@ -14,5 +14,5 @@ def lookup() -> None:
     })
 
 
-if __name__ == "__main___":
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
