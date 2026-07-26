1e50ab1597b4eb53ffb93fe88c09f2b720024201
MusangQim <musangqim03@gmail.com>
2026-07-26 23:37:34 +0800

    UPDATE: add LED for indicator

--- displayJSON/displayJSON.ino
+++ displayJSON/displayJSON.ino
@@ -10,18 +10,24 @@ Adafruit_SSD1306 display(128, 64, &Wire, -1);
 // Global Wifi Setup
 const char* ssid = "whynot_2.4GHz";
 const char* password = "this7465";
-const char* serverURL = "http://192.168.x.xx:5000/lookup"; //"https://httpbin.org/post";
+const char* serverURL = "http://192.168.0.xx:5000/lookup"; //"https://httpbin.org/post";
+const char* resultBaseURL = "http://192.168.x.xx:5000/result/";
 
 //Global touch sensor + menu
-String usernames[] = {"musangqim", "testuser", "johnwick"};
+String usernames[] = {"musangqim", "musang_qim", "musangQim"};
 int totalUser = 3;
 int selectedIndex = 0;
 enum State {SELECTING, SEARCHING, RESULT};
 State currentState = SELECTING;
-
 const int PIN_UP = 18;
 const int PIN_SELECT = 5;
 const int PIN_DOWN = 4;
+
+//Global LED
+const int LED_R = 19;
+const int LED_G = 21;
+unsigned long lastBlinkTime = 0;
+bool ledRState = false;
 
 String job_id = "";
 bool searchDone = false;
@@ -59,7 +65,8 @@ void loop()
   switch (currentState)
   {
     case SELECTING:
-    // choose username
+    {
+      // choose username
       display.clearDisplay();
       display.setCursor(10, 10);
       display.println("Select Username:");
@@ -83,78 +90,127 @@ void loop()
         delay(300);
       }
       break;
+    }
     case SEARCHING:
-    // code WiFi check + POST + polling loop
-      break;
-    case RESULT:
-    // code display result + scroll
-      break;
-  }
-  display.clearDisplay();
-  if (WiFi.status() == WL_CONNECTED)
-  {
-    HTTPClient http;
-    http.begin(serverURL);
-    String jsonPayload = "{\"username\": \"musangqim\"}";
-    http.addHeader("Content-Type", "application/json");
-    int httpCode = http.POST(jsonPayload);
-    if (httpCode == 200)
     {
-      String response = http.getString();
-      DynamicJsonDocument doc(1024);
-      DeserializationError error = deserializeJson(doc, response);
-      job_id = doc["job_id"].as<String>();
-      if (error)
+      if (WiFi.status() != WL_CONNECTED)
       {
-        display.setCursor(10, 32);
-        display.println("JSON Parse Error");
+        display.clearDisplay();
+        display.setCursor(10, 20);
+        display.println("WiFi not Connected");
         display.display();
         delay(2000);
-        return;
+        currentState = SELECTING;
+        break;
       }
       display.clearDisplay();
       Serial.println("Searching.....");
       display.setCursor(10, 20);
       display.println(("Searching....."));
       display.display();
-    }
-    else if (httpCode > 0)
-    {
-      Serial.println("Server responded but with error code: " + String(httpCode));
-    }
-    else
-    {
-      Serial.println("Connection Failed, Error: " + String(httpCode));
-    }
-    http.end();
-    while (searchDone == false)
-    {
-      delay(5000);
-      String resultURL = "http://192.168.x.xx:5000/result/" + job_id;
-      HTTPClient http2;
-      http2.begin(resultURL);
-      int httpCode2 = http2.GET();
-      if (httpCode2 == 200)
+
+      // PART POST /lookup
+      HTTPClient http;
+      http.begin(serverURL);
+      String jsonPayload = "{\"username\": \"" + usernames[selectedIndex] + "\"}";
+      http.addHeader("Content-Type", "application/json");
+      int httpCode = http.POST(jsonPayload);
+
+      if (httpCode != 200)
       {
-        String response2 = http2.getString();
-        DynamicJsonDocument doc2(1024);
-        DeserializationError error2 = deserializeJson(doc2, response2);
-        String status = doc2["status"];
-        if (status == "done")
+        Serial.println("POST Failed, code: " + String(httpCode));
+        http.end();
+        display.clearDisplay();
+        display.setCursor(10, 20);
+        display.println("POST Error: " + String(httpCode));
+        display.display();
+        delay(200);
+        currentState = SELECTING;
+        break;
+      }
+
+      String response = http.getString();
+      http.end();
+
+      DynamicJsonDocument doc(1024);
+      DeserializationError error = deserializeJson(doc, response);
+      if (error)
+      {
+        display.setCursor(10, 32);
+        display.println("JSON Parse Error");
+        display.display();
+        delay(2000);
+        currentState = SELECTING;
+        break;
+      }
+      job_id = doc["job_id"].as<String>();
+      
+      // PART POLLING LOOP /result/job_id
+      while (searchDone == false)
+      {
+        delay(5000);
+        String resultURL = String(resultBaseURL) + job_id;
+        HTTPClient http2;
+        http2.begin(resultURL);
+        int httpCode2 = http2.GET();
+        if (httpCode2 == 200)
         {
-          JsonArray sites = doc2["sites_found"];
-          int count = doc2["count"];
-          Serial.println("Success! Response: " + response2);
-          display.setCursor(10, 10);
-          display.println("Found: " + String(count) + " sites");
-          display.setCursor(10, 20);
-          display.println("Found: " + String(sites));
-          display.display();
-          searchDone = true;
+          String response2 = http2.getString();
+          DynamicJsonDocument doc2(2048);
+          DeserializationError error2 = deserializeJson(doc2, response2);
+          if (!error2)
+          {
+            String status = doc2["status"];
+            if (status == "done")
+            {
+              JsonArray sites = doc2["sites_found"];
+              totalSitesFound = doc2["count"];
+              Serial.println("Success! Response: " + response2);
+              for (int i = 0; i < totalSitesFound && i < 20; i++)
+                sitesList[i] = sites[i].as<String>();
+              searchDone = true;
+              scrollIndex = 0;
+              currentState = RESULT;
+            }
+          }
+        }
+        http2.end();
+      }
+      break;
+    }
+    case RESULT:
+    {
+      display.clearDisplay();
+      display.setCursor(10, 10);
+      display.println("Found: " + String(totalSitesFound) + " sites");
+      for (int i = 0; i < 3; i++)
+      {
+        int siteNum = scrollIndex + i;
+        if (siteNum < totalSitesFound)
+        {
+          display.setCursor(10, 20 + (i * 12));
+          display.println("> " + sitesList[siteNum]);
         }
       }
-      http2.end();
+      display.display();
+      if (digitalRead(PIN_UP) == HIGH)
+      {
+        if (scrollIndex > 0)
+          scrollIndex = scrollIndex - 1;
+        delay(300);
+      }
+      if (digitalRead(PIN_DOWN) == HIGH)
+      {
+        if (scrollIndex < totalSitesFound - 3)
+        scrollIndex = scrollIndex + 1;
+        delay(300);
+      }
+      if (digitalRead(PIN_SELECT) == HIGH)
+      {
+        currentState = SELECTING;
+        delay(300);
+      }
+      break;
     }
   }
-  delay(8000);
 }
